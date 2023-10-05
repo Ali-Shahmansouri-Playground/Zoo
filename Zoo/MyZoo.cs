c1e063794d339511cd0a7ea2225213e7887b6b5e
Ali-Shahmansouri-Playground <ali.shahmansouri110@gmail.com>
2023-10-05 22:14:42 +0330

    Refactor Zoo class name and AddAnimal method param

---
+++ Zoo/MyZoo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zoo.Animals;
+
+namespace Zoo
+{
+    public sealed class MyZoo
+    {
+        private readonly List<Animal> animals;
+
+        public MyZoo()
+        {
+            animals = new List<Animal>();
+        }
+
+        public void AddAnimal(Animal[] args)
+        {
+            foreach (var animal in args)
+            {
+                animals.Add(animal);
+            }
+        }
+
+        public void GetAnimalDetails()
+        {
+            //TODO: Implement
+        }
+    }
+}
