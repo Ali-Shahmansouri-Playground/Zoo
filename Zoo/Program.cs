0a43d6589aeaa5ac5db89f2d295c72cf2eebf0f8
Ali-Shahmansouri-Playground <ali.shahmansouri110@gmail.com>
2023-10-05 22:24:03 +0330

    Initialize initial zoo animals

--- Zoo/Program.cs
+++ Zoo/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using Zoo;
+using Zoo.Animals;
+
+var myZoo = new MyZoo();
+
+
+myZoo.AddAnimal(new Animal[]
+{
+ new Cat(new DateTime(2000, 1, 2)),
+ new Crocodile(new DateTime(2001, 2, 3)),
+ new Fish(new DateTime(2002, 3, 4)),
+ new Lion(new DateTime(2003, 5, 6)),
+ new Turtle(new DateTime(2004, 6, 7))
+});
